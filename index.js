59b15f1c47b6aa6d3c0de01f38c744f2cecbf368
talz-a <talal.gl2004@gmail.com>
2025-02-15 18:16:00 -0500

    fixed formatting

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ let mouseOverContainer = document.getElementsByTagName("body")[0];
 let face = document.getElementById("window");
 
 function transforms(x, y, el) {
-  let box = el.getBoundingClientRect();
-  let calcX = -(y - box.y - box.height / 2) / constraint;
-  let calcY = (x - box.x - box.width / 2) / constraint;
-  return (
-    "perspective(100px) " +
-    "   rotateX(" +
-    calcX +
-    "deg) " +
-    "   rotateY(" +
-    calcY +
-    "deg) "
-  );
+    let box = el.getBoundingClientRect();
+    let calcX = -(y - box.y - box.height / 2) / constraint;
+    let calcY = (x - box.x - box.width / 2) / constraint;
+    return (
+        "perspective(100px) " +
+        "   rotateX(" +
+        calcX +
+        "deg) " +
+        "   rotateY(" +
+        calcY +
+        "deg) "
+    );
 }
 
 function transformElement(el, xyEl) {
-  el.style.transform = transforms.apply(null, xyEl);
+    el.style.transform = transforms.apply(null, xyEl);
 }
 
 mouseOverContainer.onmousemove = (e) => {
-  let xy = [e.clientX, e.clientY];
-  let position = xy.concat([face]);
+    let xy = [e.clientX, e.clientY];
+    let position = xy.concat([face]);
 
-  window.requestAnimationFrame(() => {
-    transformElement(face, position);
-  });
+    window.requestAnimationFrame(() => {
+        transformElement(face, position);
+    });
 };
