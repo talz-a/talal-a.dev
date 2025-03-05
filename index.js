68cb8c3036e33dc27381ed903e245d9ee2071e03
talz-a <talal.gl2004@gmail.com>
2025-03-05 09:46:39 -0500

    updated return of transforms function

--- index.js
+++ index.js
@@ -6,15 +6,7 @@ function transforms(x, y, el) {
     let box = el.getBoundingClientRect();
     let calcX = -(y - box.y - box.height / 2) / constraint;
     let calcY = (x - box.x - box.width / 2) / constraint;
-    return (
-        "perspective(100px) " +
-            "   rotateX(" +
-            calcX +
-            "deg) " +
-            "   rotateY(" +
-            calcY +
-            "deg) "
-    );
+    return `perspective(100px) rotateX(${calcX}deg) rotateY(${calcY}deg)`;
 }
 
 function transformElement(el, xyEl) {
