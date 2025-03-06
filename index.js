d5f8b80d4b4876d51684ff81b60f1de5cda68cf2
talz-a <talal.gl2004@gmail.com>
2025-03-05 19:31:45 -0500

    updated variables to const

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let mouseOverContainer = document.getElementsByTagName("body")[0];
 let face = document.getElementById("window");
 
 function transforms(x, y, el) {
-    let box = el.getBoundingClientRect();
-    let calcX = -(y - box.y - box.height / 2) / constraint;
-    let calcY = (x - box.x - box.width / 2) / constraint;
+    const box = el.getBoundingClientRect();
+    const calcX = -(y - box.y - box.height / 2) / constraint;
+    const calcY = (x - box.x - box.width / 2) / constraint;
     return `perspective(100px) rotateX(${calcX}deg) rotateY(${calcY}deg)`;
 }
 
@@ -14,9 +14,8 @@ function transformElement(el, xyEl) {
 }
 
 mouseOverContainer.onmousemove = (e) => {
-    let xy = [e.clientX, e.clientY];
-    let position = xy.concat([face]);
-
+    const xy = [e.clientX, e.clientY];
+    const position = xy.concat([face]);
     window.requestAnimationFrame(() => {
         transformElement(face, position);
     });
