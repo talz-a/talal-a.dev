6aa431cd9b8289c06ce8a9fd11497396d73b7420
talz-a <talal.gl2004@gmail.com>
2025-04-09 22:17:32 -0400

    reimplmented basic css and hover animate

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ let mouseOverContainer = document.getElementsByTagName("body")[0];
 let face = document.getElementById("window");
 
 function transforms(x, y, el) {
-    const box = el.getBoundingClientRect();
-    const calcX = -(y - box.y - box.height / 2) / constraint;
-    const calcY = (x - box.x - box.width / 2) / constraint;
-    return `perspective(100px) rotateX(${calcX}deg) rotateY(${calcY}deg)`;
+  const box = el.getBoundingClientRect();
+  const calcX = -(y - box.y - box.height / 2) / constraint;
+  const calcY = (x - box.x - box.width / 2) / constraint;
+  return `perspective(100px) rotateX(${calcX}deg) rotateY(${calcY}deg)`;
 }
 
 function transformElement(el, xyEl) {
-    el.style.transform = transforms.apply(null, xyEl);
+  el.style.transform = transforms.apply(null, xyEl);
 }
 
 mouseOverContainer.onmousemove = (e) => {
-    const xy = [e.clientX, e.clientY];
-    const position = xy.concat([face]);
-    window.requestAnimationFrame(() => {
-        transformElement(face, position);
-    });
+  const xy = [e.clientX, e.clientY];
+  const position = xy.concat([face]);
+  window.requestAnimationFrame(() => {
+    transformElement(face, position);
+  });
 };
