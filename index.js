45d7de6dd4f7c2378f98df1ff663ebb6e2d1ad21
talz-a <talal.gl2004@gmail.com>
2025-04-14 16:55:14 -0400

    Added slide in animation and updated buttons + work-items hover

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-document.body.onmousemove = (e) => {
-  const constraint = 40;
-  const face = document.querySelector(".penguin-drawing");
+(() => {
+  document.body.onmousemove = (e) => {
+    const constraint = 40;
+    const face = document.querySelector(".penguin-drawing");
+    window.requestAnimationFrame(() => {
+      const { x, y, width, height } = face.getBoundingClientRect();
+      const rotateX = -((e.clientY - y - height / 2) / constraint);
+      const rotateY = (e.clientX - x - width / 2) / constraint;
+      face.style.transform = `perspective(100px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    });
+  };
 
-  window.requestAnimationFrame(() => {
-    const { x, y, width, height } = face.getBoundingClientRect();
-    const rotateX = -((e.clientY - y - height / 2) / constraint);
-    const rotateY = (e.clientX - x - width / 2) / constraint;
-    face.style.transform = `perspective(100px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-  });
-};
-
-document.querySelectorAll(".work-item").forEach((item) =>
-  item.addEventListener("mouseenter", () => {
-    document.querySelector(".hover-bg").style.transform =
-      `translateY(${item.offsetTop}px)`;
-  }),
-);
+  document.querySelectorAll(".work-item").forEach((item) =>
+    item.addEventListener("mouseenter", () => {
+      document.querySelector(".hover-bg").style.transform =
+        `translateY(${item.offsetTop}px)`;
+    }),
+  );
+})();
