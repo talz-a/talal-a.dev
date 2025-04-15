a9614a722efbc0c34859f4fd1fec462a3bd16c72
talz-a <talal.gl2004@gmail.com>
2025-04-14 21:31:39 -0400

    added in pfp and finished projects section

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 (() => {
   document.body.onmousemove = (e) => {
     const constraint = 40;
-    const face = document.querySelector(".penguin-drawing");
+    const face = document.querySelector(".pfp-icon");
     window.requestAnimationFrame(() => {
       const { x, y, width, height } = face.getBoundingClientRect();
       const rotateX = -((e.clientY - y - height / 2) / constraint);
@@ -9,11 +9,16 @@
       face.style.transform = `perspective(100px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
     });
   };
-
   document.querySelectorAll(".work-item").forEach((item) =>
     item.addEventListener("mouseenter", () => {
-      document.querySelector(".hover-bg").style.transform =
+      document.querySelector(".work-hover-bg").style.transform =
         `translateY(${item.offsetTop}px)`;
+    }),
+  );
+  document.querySelectorAll(".project-card").forEach((item) =>
+    item.addEventListener("mouseenter", () => {
+      document.querySelector(".project-hover-bg").style.transform =
+        `translateX(${item.offsetLeft}px)`;
     }),
   );
 })();
